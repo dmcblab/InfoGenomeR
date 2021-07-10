163c267cce3f7c64b36d1bc307c303c2af2c6605
172 <qlalf1457@gist.ac.kr>
2021-07-10 19:04:38 +0900

    v1.1

--- breakpoint_graph/breakpoint_graph.sh
+++ breakpoint_graph/breakpoint_graph.sh
@@ -1,56 +1,171 @@
 #!/bin/bash
-mode="somatic"
-SV="SVs"
-#tmp_folder="$PWD/tmp/"
-#min_ploidy="1.5";
-#max_ploidy="5";
-###########
-library="/home/qlalf1457/InfoGenomer/TOTAL"
-lambda_ini="1"
-lambda_fi="4"
-bicseq="~/NBICseq-seg_v0.7.2/NBICseq-seg.pl"
-#fasta_prefix="/DATA1/AITL/hg19/ucsc.hg19"
-#mean_phy_depth=5;
-#########################
+lambda_ini=1
+lambda_fi=4
 search_length=50000;
 CIGAR="100M";
 read_length=100;
-###########################
-if [ $# != 14 ]; then
-        echo $0 \<mode\> \<sample\> \<cancer_type\> \<min_ploidy\> \<max_ploidy\> \<NPE_dir\> \<tumor_bin_dir\> \<germ_bin_dir\> \<germ_LocSeq_result\> \<fasta_prefix\> \<haplotype_coverage\> \<bam\> \<normal_bam\> \<chr_prefix\>
+min_ploidy=1.5
+max_ploidy=5
+sample=sample1
+cancer_type=null
+npe_dir=null
+fasta_prefix="hg19"
+out_dir=InfoGenomeR_job
+chr_prefix=0  # The reference genome has "chr" prefix or not
+
+bam=null
+normal_bam=null
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -m|--mode)
+      mode="$2"
+      shift
+      shift
+      ;;
+    -i|--lambda_ini)
+      lambda_ini="$2"
+      shift
+      shift
+      ;;
+    -f|--lambda_fi)
+      lambda_fi="$2"
+      shift
+      shift
+      ;;
+    -t|--cancer_type)
+      cancer_type="$2"
+      shift
+      shift
+      ;;
+    -d|--npe_dir)
+      npe_dir=`readlink -f "$2"`
+      shift
+      shift
+      ;;
+    -g|--ref_genome)
+      fasta_prefix=`readlink -f "$2"`
+      shift
+      shift
+      ;;
+    -c|--cn_norm_germ)
+      cn_norm_germ=`readlink -f "$2"`
+      shift
+      shift
+      ;;
+    -s|--germ_LocSeq_result)
+      germ_LocSeq_result=`readlink -f "$2"`
+      shift
+      shift
+      ;;
+    -o|--out_dir)
+      out_dir="$2"
+      shift
+      shift
+      ;;
+    -h|--help)
+      echo -e "Usage: ./breakpoint_graph.sh <SVs> <cn_norm> [options]\n"
+      echo -e "Options:"
+      echo -e  "\t-m, --mode (required)\n \t\t Select the mode (germline, total, somatic)
+\t-i, --lambda_ini (required) \n \t\t Initial lambda for the first round iterations (default: 1)
+\t-f, --lambda_fi (required) \n \t\t Final lambda for the second round iterations (default: 4)
+\t-t, --cancer_type (optional) \n \t\t Cancer type used for ABSOLUTE estimation (BRCA, GBM, OV, ...). If unknown, write null.
+\t-d, --npe_dir (optional) \n \t\t Directory that contains NPE.fq1 and NPE.fq2 (non-properly paired reads). If it is not assigned, InfoGenomeR runs without NP reads mapping.
+\t-g, --ref_genome (required for NP reads mapping) \n \t\t Fasta prefix (hg19 or hg38). Enter the prefix without .2bit and .fa extension.
+\t-c, cn_norm_germ (required for somatic mode) \n \t\t Directory that contains copy number bins from a control genome.
+\t-s, --germ_LocSeq_result (required for somatic mode) \n \t\t Local segmentation results from a control genome.
+\t-o, --out_dir \n \t\t If it already exists, results are overlaid (default: InfoGenomeR_job)
+\t-h, --help\n
+"
+      
+      exit 1
+      ;;
+    *)  
+     POSITIONAL+=("$1") # save it in an array for later
+     shift
+     ;;
+  esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo "InfoGenomeR_lib = ${InfoGenomeR_lib}"
+if [[ ! -s ${InfoGenomeR_lib}/breakpoint_graph ]];then
+	echo "set the InfoGenomeR_lib path correctly"
+	exit 1
+fi
+echo "BICseq2_path = ${BICseq2_path}"
+if [[ ${BICseq2_path} == "" ]];then
+        echo "set the InfoGenomeR_lib path"
         exit 1
 fi
-mode=$1
-sample=$2
-cancer_type=$3
-min_ploidy=$4
-max_ploidy=$5
-NPE_dir=`readlink -f $6`
-tumor_bin_dir=`readlink -f $7`
-germ_bin_dir=`readlink -f $8`
-germ_LocSeq_result=`readlink -f $9`
-fasta_prefix=`readlink -f ${10}`
-haplotype_coverage=${11}
-#bam=${12}
-#normal_bam=${13}
-bam=`readlink -f ${12}`
-normal_bam=`readlink -f ${13}`
-chr_prefix=${14}
+echo "mode = ${mode}"
+if [[ ${mode} == "" ]];then
+	echo "set the mode; total, germline, or somatic"
+	exit 1
+fi
+echo "lambda_ini  = ${lambda_ini}"
+echo "lambda_fi   = ${lambda_fi}"
+echo "cancer_type = ${cancer_type}"
+if [[ -s $npe_dir\/NPE.fq1 ]] && [[ -s $npe_dir\/NPE.fq2 ]];then
+	echo "NP reads mapping = True"
+else
+	npe_dir="null"
+	echo "NPE_mapping = False"
+fi
+echo "NPE_dir = ${npe_dir}"
+echo "fasta_prefix = ${fasta_prefix}"
+if [[ -s $npe_dir ]];then
+	if [[ ! -s ${fasta_prefix}.fa ]] || [[ ! -s ${fasta_prefix}.2bit ]];then
+		echo "write the reference genome prefix and check .2bit exists"
+		exit 1
+	fi
+fi
+SV=`readlink -f "$1"`
+echo -e "SVs = $SV"
+if [[ $1 == "" ]];then
+	echo "An SV file is required"
+	exit 1
+fi
+cn_norm=`readlink -f "$2"`
+echo "cn_norm = $cn_norm"
+if [[ $2 == "" ]];then
+        echo "cn_norm directory is required"
+        exit 1
+fi
+echo "cn_norm_germ = ${cn_norm_germ}"
+echo "germ_LocSeq_result = ${germ_LocSeq_result}"
+if [[ $mode == "somatic" ]];then
+	if [[ $cn_norm_germ == "" ]];then
+		echo "cn_norm_germ directory is required";
+		exit 1
+	fi
+        if [[ $germ_LocSeq_result == "" ]];then
+                echo "germ_LocSeq_result is required";
+                exit 1
+        fi
+fi
 
-
-bin="$tumor_bin_dir/"
-normal_bin="$germ_bin_dir/"
-#chr_prefix="1"
+bin="$cn_norm/"
+normal_bin="$cn_norm_germ/"
+if [[ -s $out_dir ]];then
+	echo "out_dir exists... overlapping"
+fi
+mkdir -p $out_dir
+cd $out_dir
+cp $SV ./SVs
+SV=SVs
 i=1;
 iter=1;
-
+library=$InfoGenomeR_lib\/breakpoint_graph
+bicseq=$BICseq2_path\/NBICseq-seg.pl
 if [ $mode == "germline" ];then
 	lambda_ini=1;
-#	bin="$working_dir/bicseq_norm/"
 	Rscript $library/SV_break_adjustment.R
 	$library/SV_truncated_wgs.sh SVs $bin
 	Rscript $library/SV_break_adjust.R
-	mkdir tmp;
+	mkdir -p tmp;
 	tmp_folder="$PWD/tmp/"
         Rscript $library/SV_local_CN_segment_wgs_raw.R $SV $i $bin $tmp_folder $bicseq $lambda_ini $bam $chr_prefix;
 	rm -rf tmp;
@@ -60,15 +175,16 @@ fi
 
 
 if [ $mode == "somatic" ];then
-	mkdir bicseq_norm
-	mkdir bicseq_norm_germ
-
+	mkdir -p cn_norm
+	mkdir -p cn_norm_germ
+	echo "filtering germline bins"
 	for chr in {1..23}; do
 		Rscript $library/germ_cnv_filter_v2.R $germ_LocSeq_result $normal_bin $bin $chr
+		echo "$chr.bin"
 	done
 	
-	bin="$PWD/bicseq_norm/"
-	normal_bin="$PWD/bicseq_norm_germ/"
+	bin="$PWD/cn_norm/"
+	normal_bin="$PWD/cn_norm_germ/"
 fi
 
 
@@ -78,29 +194,37 @@ $library/SV_truncated_wgs.sh SVs $bin
 
 ABSOLUTE_STABLE="F";
 
-for iter in {1..50}
+ROUND=1
+for iter in {1..100}
 do
-	mkdir iter$iter;
+	mkdir -p iter$iter;
 	cd iter$iter;
 	cp  ../SVs ./
-	mkdir tmp;
+	mkdir -p tmp;
 
+	if [[ $ROUND -eq 1 ]];then
+		lambda=$lambda_ini
+	else
+		lambda=$lambda_fi
+	fi
+
+	echo "Local segmentation..."
 	abs_line=50000;
         while [ $abs_line -ge 50000 ];do
 
 
 		if [ $mode == "somatic" ];then
-	        	mkdir tmp_tumor;
-		        mkdir tmp_normal;
+	        	mkdir -p tmp_tumor;
+		        mkdir -p tmp_normal;
 		        tmp_folder="$PWD/tmp/"
 		        tmp_folder1="$PWD/tmp_tumor/"
 		        tmp_folder2="$PWD/tmp_normal/"
-		        Rscript $library/SV_local_CN_segment_wgs_raw_somatic.R $SV $i $bin $tmp_folder $bicseq $lambda_ini $bam $normal_bin $tmp_folder1 $tmp_folder2 $normal_bam $chr_prefix
+		        Rscript $library/SV_local_CN_segment_wgs_raw_somatic.R $SV $i $bin $tmp_folder $bicseq $lambda $bam $normal_bin $tmp_folder1 $tmp_folder2 $normal_bam $chr_prefix
 			rm -rf tmp_tumor;
 			rm -rf tmp_normal;
 		else
 			tmp_folder="$PWD/tmp/"
-			Rscript $library/SV_local_CN_segment_wgs_raw.R $SV $i $bin $tmp_folder $bicseq $lambda_ini $bam $chr_prefix;
+			Rscript $library/SV_local_CN_segment_wgs_raw.R $SV $i $bin $tmp_folder $bicseq $lambda $bam $chr_prefix;
 		fi
 	
 		abs_line=`cat copy_numbers | wc -l`
@@ -133,72 +257,37 @@ do
 	
 	cp SVs.CN_opt.filtered ../SVs
 	cd ../
-	if [ $test1 -eq $test2 ];then
+	if [[ $test1 -eq $test2 ]] && [[ $ROUND -eq 1 ]];then
+		if [[ -s $npe_dir ]];then
+			cd iter$iter
+			total_coverage_h=`cat $bin/*.norm.bin  | awk 'BEGIN{sum=0;n=0;}{sum=sum+($2-$1)*$3/100; n=n+1}END{print sum/n}'`
+			ploidy_h=`cat ABSOLUTE_output/output/reviewed/*.test.ABSOLUTE.table.txt | awk -F "\t" '{print $5}' | tail -n 1`
+			purity_h=`cat ABSOLUTE_output/output/reviewed/*.test.ABSOLUTE.table.txt | awk -F "\t" '{print $4}' | tail -n 1`
+			bc_h=`echo -e "$total_coverage_h\t$ploidy_h\t$purity_h" | awk '{b=$1/$2*$3; print (b-int(b)<0.499)?int(b):int(b)+1}'`
+			base_coverage=$bc_h
+			Rscript $library/remapping/unbalanced_nodes.R
+			perl $library/remapping/edge_filling_job_optimized.pl $fasta_prefix.fa $npe_dir $base_coverage $library $search_length $CIGAR $read_length $chr_prefix> edge_probability
+			perl $library/remapping/edge_filling_job_blat_recal.pl $fasta_prefix
+			perl $library/remapping/edge_filling_job_blat_recal_sam_To_bed_re.pl $fasta_prefix.fa $CIGAR $read_length $search_length > edge_filling_sorted_paired_primary.sam.info.recal
+			perl $library/remapping/edge_filling_job_optimized_for_recal_info.pl $library $base_coverage $search_length > edge_probability_recal
+			sort -nrk 5,5 edge_probability_recal  > edge_probability_recal_sorted
+			cat  edge_probability_recal_sorted | awk '{if($5>0.01) print $0}' > edge_probability_recal_sorted.filtered
+			cp SVs SVs.added
+			perl $library/remapping/module_edge_to_SVs.pl $search_length >> SVs.added
+			cp SVs.added ../SVs
+			cd ../
+		fi
+		if [[ ! -s iter$iter\/SVs.added ]];then
+                        cd iter$iter
+			Rscript $library/TO_bpgraph_hidden_node_for_connectivity.R
+			Rscript $library/lite_SV_edges.R ../iter1/SVs $search_length F
+			cp SVs.added ../SVs
+			cd ../
+
+		fi
+		ROUND=2
+	elif [[ $test1 -eq $test2 ]];then
+                echo "Breakpoint graph construction is finished"
 		break;
 	fi
-done
-
-
-cd iter$iter
-
-	Rscript $library/remapping/unbalanced_nodes.R
-	perl $library/remapping/edge_filling_job_optimized.pl $fasta_prefix.fa $NPE_dir $haplotype_coverage $library $search_length $CIGAR $read_length $chr_prefix> edge_probability
-	perl $library/remapping/edge_filling_job_blat_recal.pl $fasta_prefix
-	perl $library/remapping/edge_filling_job_blat_recal_sam_To_bed_re.pl $fasta_prefix.fa $CIGAR $read_length $search_length > edge_filling_sorted_paired_primary.sam.info.recal
-	perl $library/remapping/edge_filling_job_optimized_for_recal_info.pl $library $haplotype_coverage $search_length > edge_probability_recal
-	sort -nrk 5,5 edge_probability_recal  > edge_probability_recal_sorted
-	cat  edge_probability_recal_sorted | awk '{if($5>0.01) print $0}' > edge_probability_recal_sorted.filtered
-	cp SVs SVs.added
-	perl $library/remapping/module_edge_to_SVs.pl $search_length >> SVs.added
-	cp SVs.added ../SVs
-cd ../
-
-
-iter_start=$iter+1;
-
-for (( iter=$iter_start; iter<=100; iter++ ))
-do
-        mkdir iter$iter;
-        cd iter$iter;
-        cp  ../SVs ./
-        mkdir tmp;
-
-        if [ $mode == "somatic" ];then
-                mkdir tmp_tumor;
-                mkdir tmp_normal;
-                tmp_folder="$PWD/tmp/"
-                tmp_folder1="$PWD/tmp_tumor/"
-                tmp_folder2="$PWD/tmp_normal/"
-                Rscript $library/SV_local_CN_segment_wgs_raw_somatic.R $SV $i $bin $tmp_folder $bicseq $lambda_fi $bam $normal_bin $tmp_folder1 $tmp_folder2 $normal_bam $chr_prefix
-		rm -rf tmp_tumor;
-		rm -rf tmp_normal;
-
-        else
-                tmp_folder="$PWD/tmp/"
-                Rscript $library/SV_local_CN_segment_wgs_raw.R $SV $i $bin $tmp_folder $bicseq $lambda_fi $bam $chr_prefix;
-        fi
-	rm -rf tmp;
-
-        Rscript $library/ABSOLUTE.R $cancer_type $sample $min_ploidy $max_ploidy $ABSOLUTE_STABLE
-        if [[ -s ABSOLUTE_output/output/reviewed/sample1.test.ABSOLUTE.table.txt ]];then
-                echo "pass"
-        else
-                ABSOLUTE_STABLE="T";
-                Rscript $library/ABSOLUTE.R $cancer_type $sample $min_ploidy $max_ploidy $ABSOLUTE_STABLE
-                if [[ -s ABSOLUTE_output/output/reviewed/sample1.test.ABSOLUTE.table.txt ]];then
-                        echo "pass"
-                else
-                        exit 1
-                fi
-        fi
-
-        Rscript $library/integer_programming.R $sample "copy_numbers_ABSOLUTE_input.negative_marker" $SV $read_length > IP.log
-        test1=`cat SVs.CN_opt.filtered | wc -l`
-        test2=`cat SVs | wc -l`
-
-        cp SVs.CN_opt.filtered ../SVs
-        cd ../
-        if [ $test1 -eq $test2 ];then
-                break;
-        fi
 done
---
+++ breakpoint_graph/lite_SV_edges.R
@@ -0,0 +1,96 @@
+args=commandArgs(T);
+thres=as.numeric(args[2]);
+t=read.table("unsatisfied.list.no_telomere_ends",stringsAsFactors=F)
+sv=read.table("SVs.CN_opt.filtered",stringsAsFactors=F);
+
+t=t[t[,5]==0,];
+raw_sv=read.table(args[1],stringsAsFactors=F);
+one_side= if (args[3]=="T") T else F
+
+raw_sv$ori1=NA;
+raw_sv$ori2=NA;
+
+for(i in 1:nrow(raw_sv)){
+	raw_sv$ori1[i]=strsplit(raw_sv[i,6],"to")[[1]][1];
+	raw_sv$ori2[i]=strsplit(raw_sv[i,6],"to")[[1]][2];
+}
+if(file.exists("cluster_sv")){
+	if(file.info("cluster_sv")$size!=0){
+		cluster_sv=read.table("cluster_sv",stringsAsFactors=F);
+	}
+}
+
+
+sv_rbind = function(l_sv, add_sv){
+
+	for(add_i in 1:nrow(add_sv)){
+		w=which((l_sv[,2] == add_sv[add_i,2] & l_sv[,3] == add_sv[add_i,3] )|
+		(l_sv[,2] == add_sv[add_i,4] & l_sv[,3] == add_sv[add_i,5])|
+	       ( l_sv[,4] == add_sv[add_i,2] & l_sv[,5] == add_sv[add_i,3])|
+	       ( l_sv[,4] == add_sv[add_i,4] & l_sv[,5] == add_sv[add_i,5]) );
+		if(length(w)!=0){
+			l_sv = l_sv[-w,];
+			l_sv = rbind(l_sv, add_sv[add_i,1:14]);
+		}else{
+	                l_sv = rbind(l_sv, add_sv[add_i,1:14]);
+		}
+	}
+	return(l_sv);
+}
+
+
+
+
+
+
+if(nrow(t)>0){
+	t$ori_tag=NA;
+	t[t[,3] %% 2 == 1, "ori_tag"]=5;
+	t[t[,3] %% 2 == 0, "ori_tag"] = 3;
+
+	for(i in 1:nrow(t)){
+			tmp=raw_sv[(t[i,1]==raw_sv[,2] & abs(t[i,2]-raw_sv[,3]) < thres & raw_sv[,"ori1"] == t[i,"ori_tag"]) | (t[i,1] == raw_sv[,4] & abs(t[i,2] - raw_sv[,5]) < thres & raw_sv[,"ori2"] == t[i,"ori_tag"]),];
+
+			if(nrow(tmp)!=0){
+				for( j in 1:nrow(tmp)){
+					if(exists("cluster_sv")){
+						tmp2=cluster_sv[ (tmp[j,2] == cluster_sv[,2] & tmp[j,3] == cluster_sv[,3]) | (tmp[j,2] == cluster_sv[,4] & tmp[j,3] == cluster_sv[,5]), 15][1];
+						if(!is.na(tmp2)){
+							sv=sv_rbind(sv,cluster_sv[cluster_sv[,15] == tmp2,1:14]);
+						}else{
+							if( one_side ||   (length(which(t[,1]== tmp[j,2]  &abs( t[,2]- tmp[j,3] ) < thres & t[,"ori_tag"] == tmp[j,"ori1"] ))!=0 &
+	                                                        length(which(t[,1]== tmp[j,4]  &abs( t[,2]- tmp[j,5] ) < thres & t[,"ori_tag"] == tmp[j,"ori2"] ))!=0 )
+							){
+								sv=sv_rbind(sv, tmp[j,1:14]);
+							}
+						}
+
+					}else{
+						if(one_side || length(which(t[,1]== tmp[j,2]  &abs( t[,2]- tmp[j,3] ) < thres & t[,"ori_tag"] == tmp[j,"ori1"] ))!=0 &
+							length(which(t[,1]== tmp[j,4]  &abs( t[,2]- tmp[j,5] ) < thres & t[,"ori_tag"] == tmp[j,"ori2"] ))!=0
+						){
+							sv=sv_rbind(sv, tmp[j,1:14]);
+						}
+					}
+				}
+			}
+	}
+}
+
+
+
+t=sv;
+break_thres=1000;
+new_t=t[0,]
+while(nrow(t)!=0){
+        reverse=strsplit(t[1,6],split="to");
+        where=which((t$V2 == t$V2[1] &  abs(t$V3 - t$V3[1])<break_thres & t$V4 == t$V4[1] & abs(t$V5 - t$V5[1])<break_thres & t$V6 == t$V6[1])|
+                (t$V4 == t$V2[1] &  abs(t$V5 - t$V3[1])<break_thres & t$V2 == t$V4[1] & abs(t$V3 - t$V5[1])<break_thres & t$V6 == paste(reverse[[1]][2],"to",reverse[[1]][1],sep=""))
+                );
+        new_t[nrow(new_t)+1,]=t[1,];
+        t=t[-where,]
+}
+sv=new_t;
+
+
+write.table(sv, "SVs.added", sep="\t", quote=F, col.names=F, row.names=F)
